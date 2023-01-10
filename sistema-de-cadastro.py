d2fd085898fa8d326c57609475aed6093ee3354a
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-10 20:34:11 -0300

    Criação da lista

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -1,1 +1,11 @@
 # Este programa faz o cadastro de alunos
+
+alunos = []
+cursos = []
+
+aluno = input('Digite o nome do aluno:')
+alunos.append(aluno)
+aluno = input('Digite o nome do aluno:')
+alunos.append(aluno)
+
+print(alunos)
