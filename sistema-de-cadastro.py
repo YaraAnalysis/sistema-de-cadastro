33a8a3d524f4acd0c940b980af152d13c483eadb
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-12 17:15:18 -0300

    Criação da lista de telefones

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -1,6 +1,8 @@
 # Este programa faz o cadastro de alunos
 
 alunos = []
+telefones = []
+emails = []
 cursos = []
 
 aluno = input('Digite o nome do aluno: ')
@@ -13,5 +15,17 @@ cursos.append(curso)
 curso = input('Digite o curso do aluno: ')
 cursos.append(curso)
 
+tel = input('Digite o telefone do aluno: ')
+telefones.append(tel)
+tel = input('Digite o telefone do aluno: ')
+telefones.append(tel)
+
+email = input('Digite o email do aluno: ')
+emails.append(email)
+email = input('Digite o email do aluno: ')
+emails.append(email)
+
 print(alunos)
+print(telefones)
+print(emails)
 print(cursos)
