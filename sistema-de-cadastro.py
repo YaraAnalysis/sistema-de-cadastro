f88d7fab0e89c914c7ce329c084302595ee92ab2
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-11 21:45:55 -0300

    Criação da lista de cursos

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -3,9 +3,15 @@
 alunos = []
 cursos = []
 
-aluno = input('Digite o nome do aluno:')
+aluno = input('Digite o nome do aluno: ')
 alunos.append(aluno)
-aluno = input('Digite o nome do aluno:')
+aluno = input('Digite o nome do aluno: ')
 alunos.append(aluno)
 
+curso = input('Digite o curso do aluno: ')
+cursos.append(curso)
+curso = input('Digite o curso do aluno: ')
+cursos.append(curso)
+
 print(alunos)
+print(cursos)
