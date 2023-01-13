37bac10e15653721642c16b08d6e290c5160bf4e
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-12 23:40:23 -0300

    alteração na ordem dos inputs

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -7,21 +7,19 @@ cursos = []
 
 aluno = input('Digite o nome do aluno: ')
 alunos.append(aluno)
-aluno = input('Digite o nome do aluno: ')
-alunos.append(aluno)
-
 curso = input('Digite o curso do aluno: ')
 cursos.append(curso)
-curso = input('Digite o curso do aluno: ')
-cursos.append(curso)
-
 tel = input('Digite o telefone do aluno: ')
 telefones.append(tel)
-tel = input('Digite o telefone do aluno: ')
-telefones.append(tel)
-
 email = input('Digite o email do aluno: ')
 emails.append(email)
+
+aluno = input('Digite o nome do aluno: ')
+alunos.append(aluno)
+curso = input('Digite o curso do aluno: ')
+cursos.append(curso)
+tel = input('Digite o telefone do aluno: ')
+telefones.append(tel)
 email = input('Digite o email do aluno: ')
 emails.append(email)
 
