74d0978bfcf61eed3b67d5aab114b3c000550d77
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-14 20:58:22 -0300

    inclusão de cursos
    Estrutura de repetição para inclusão de mais de cursos.

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -10,15 +10,22 @@ cadastrar = input('Deseja iniciar o cadastro dos alunos? (S/N) ')
 while cadastrar == 'S':
     aluno = input('Digite o nome do aluno: ')
     alunos.append(aluno)
-    curso = input('Digite o curso do aluno: ')
-    cursos.append(curso)
     tel = input('Digite o telefone do aluno: ')
     telefones.append(tel)
     email = input('Digite o email do aluno: ')
     emails.append(email)
+    curso = input('Digite o curso do aluno: ')
+    curso_aluno.append(curso)
+    outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
+    while outro_curso == 'S':
+        i = 1
+        curso = input('Digite o curso do aluno: ')
+        curso_aluno.append(curso)
+        outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
+        i = i + 1
+    cursos.append(curso_aluno)
+    curso_aluno.clear
     cadastrar = input('Deseja cadastrar outro aluno? (S/N) ')
-
-
 
 print(alunos)
 print(telefones)
