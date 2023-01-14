46176273ff7dd47ac04b641553abdecc8c2e2a19
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-13 21:59:33 -0300

    Inclusão do laço de repetição para cadastro de vários alunos

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -4,24 +4,21 @@ alunos = []
 telefones = []
 emails = []
 cursos = []
+curso_aluno = []
 
-aluno = input('Digite o nome do aluno: ')
-alunos.append(aluno)
-curso = input('Digite o curso do aluno: ')
-cursos.append(curso)
-tel = input('Digite o telefone do aluno: ')
-telefones.append(tel)
-email = input('Digite o email do aluno: ')
-emails.append(email)
+cadastrar = input('Deseja iniciar o cadastro dos alunos? (S/N) ')
+while cadastrar == 'S':
+    aluno = input('Digite o nome do aluno: ')
+    alunos.append(aluno)
+    curso = input('Digite o curso do aluno: ')
+    cursos.append(curso)
+    tel = input('Digite o telefone do aluno: ')
+    telefones.append(tel)
+    email = input('Digite o email do aluno: ')
+    emails.append(email)
+    cadastrar = input('Deseja cadastrar outro aluno? (S/N) ')
 
-aluno = input('Digite o nome do aluno: ')
-alunos.append(aluno)
-curso = input('Digite o curso do aluno: ')
-cursos.append(curso)
-tel = input('Digite o telefone do aluno: ')
-telefones.append(tel)
-email = input('Digite o email do aluno: ')
-emails.append(email)
+
 
 print(alunos)
 print(telefones)
