6b019c906c1b679711ca901911923eacc97a00fd
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-15 13:35:14 -0300

    inclusão de cursos 2
    Inclusão de vários cursos para o mesmo aluno.

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -5,29 +5,37 @@ telefones = []
 emails = []
 cursos = []
 curso_aluno = []
+i = 0
 
-cadastrar = input('Deseja iniciar o cadastro dos alunos? (S/N) ')
+cadastrar = 'S'
 while cadastrar == 'S':
-    aluno = input('Digite o nome do aluno: ')
-    alunos.append(aluno)
+    nome = input('Digite o nome do aluno: ')
+    alunos.append(nome)
     tel = input('Digite o telefone do aluno: ')
     telefones.append(tel)
     email = input('Digite o email do aluno: ')
     emails.append(email)
+    #qtd_alunos = len(alunos)
+    #print(alunos)
+    #print(qtd_alunos)
     curso = input('Digite o curso do aluno: ')
     curso_aluno.append(curso)
+    print('curso aluno:', curso_aluno)
     outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
     while outro_curso == 'S':
-        i = 1
         curso = input('Digite o curso do aluno: ')
         curso_aluno.append(curso)
+        print('curso aluno: ', curso_aluno)
+#       cursos.insert(i, curso_aluno[:])
+#       curso_aluno.clear()
         outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
-        i = i + 1
-    cursos.append(curso_aluno)
-    curso_aluno.clear
+    i += 1
+    cursos.insert(i, curso_aluno[:])
+    curso_aluno.clear()
     cadastrar = input('Deseja cadastrar outro aluno? (S/N) ')
 
 print(alunos)
 print(telefones)
 print(emails)
-print(cursos)
+print('CURSOS ', cursos)
+print('CURSOS ALUNO ', curso_aluno)
