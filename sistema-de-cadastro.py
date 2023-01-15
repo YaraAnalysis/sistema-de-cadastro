c2e74badb60bf60e38c3e9683cadcdfa8f400f05
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-01-15 13:59:26 -0300

    Relatório
    Relatório de alunos cadastrados, com seus respectivos dados e cursos cadastrados.

--- sistema-de-cadastro.py
+++ sistema-de-cadastro.py
@@ -1,4 +1,4 @@
-# Este programa faz o cadastro de alunos
+# Este programa faz o cadastro de alunos e cursos do aluno
 
 alunos = []
 telefones = []
@@ -15,27 +15,22 @@ while cadastrar == 'S':
     telefones.append(tel)
     email = input('Digite o email do aluno: ')
     emails.append(email)
-    #qtd_alunos = len(alunos)
-    #print(alunos)
-    #print(qtd_alunos)
     curso = input('Digite o curso do aluno: ')
     curso_aluno.append(curso)
-    print('curso aluno:', curso_aluno)
+    print('Cursos do(a) aluno(a) {} cadastrados até agora: {}.'.format(nome, curso_aluno))
     outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
     while outro_curso == 'S':
         curso = input('Digite o curso do aluno: ')
         curso_aluno.append(curso)
         print('curso aluno: ', curso_aluno)
-#       cursos.insert(i, curso_aluno[:])
-#       curso_aluno.clear()
         outro_curso = input('Deseja cadastrar outro curso para o mesmo aluno? (S/N) ')
     i += 1
     cursos.insert(i, curso_aluno[:])
     curso_aluno.clear()
     cadastrar = input('Deseja cadastrar outro aluno? (S/N) ')
 
-print(alunos)
-print(telefones)
-print(emails)
-print('CURSOS ', cursos)
-print('CURSOS ALUNO ', curso_aluno)
+qtd_alunos = len(alunos)
+print('\nLista de alunos:')
+for i in range(qtd_alunos):
+    print('\nAluno: {} | Telefone: {} | E-mail: {} | Cursos: {}.'.format(alunos[i], telefones[i], emails[i], cursos[i]))
+print('\nFim do relatório')
